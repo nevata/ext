361daf6cc2cb1b00f19f2a434bac3e99fe019ed6
RICE <rice2010@yeah.net>
2019-12-10 18:35:27 +0800

    add Post function

--- handler.go
+++ handler.go
@@ -2,8 +2,13 @@ package ext
 
 import (
 	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
 )
+
+//SID sessionid
+var SID string
 
 //HandleError 错误返回
 func HandleError(w http.ResponseWriter, e error) {
@@ -27,4 +32,17 @@ func HandleSuccess(w http.ResponseWriter, data interface{}) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+//Post 用json格式发送
+func Post(url string, body io.Reader) (resp *http.Response, err error) {
+	req, err := http.NewRequest("POST", url, body)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", "application/json;charset=utf-8")
+	if SID != "" {
+		req.Header.Set("Authorization", fmt.Sprintf("DSSESSION %s", SID))
+	}
+	return http.DefaultClient.Do(req)
 }
