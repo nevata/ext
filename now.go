8ba4f5b7cad7ebd4d4b28f1763b1c7c4cc94dea8
rice <rice2010@yeah.net>
2020-05-08 13:47:18 +0800

    Changes to be committed: 	deleted:    now.go 	new file:   time.go

--- now.go
+++
@@ -1,8 +0,0 @@
-package ext
-
-import "time"
-
-//NowToStr 返回格式化的时间字符串
-func NowToStr() string {
-	return time.Now().Format("2006-01-02 15:04:05")
-}
---
+++ time.go
@@ -0,0 +1,20 @@
+package ext
+
+import "time"
+
+const chinaLayout = "2006-01-02 15:04:05"
+
+//NowToStr 返回当前时间的格式化字符串
+func NowToStr() string {
+	return TimeToStr(time.Now())
+}
+
+//TimeToStr 返回时间的格式化字符串
+func TimeToStr(t time.Time) string {
+	return t.Format(chinaLayout)
+}
+
+//StrToTime 解析时间字符串
+func StrToTime(s string) (time.Time, error) {
+	return time.Parse(chinaLayout, s)
+}
