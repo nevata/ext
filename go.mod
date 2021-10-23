eafb033b6210c24a27629de88173564b66311a0d
rice <rice2010@yeah.net>
2021-10-24 00:13:03 +0800

    Changes to be committed: 	new file:   go.mod 	new file:   go.sum 	modified:   model.go

---
+++ go.mod
@@ -0,0 +1,11 @@
+module ext
+
+go 1.13
+
+require (
+	github.com/gin-gonic/gin v1.7.4
+	github.com/nevata/session v0.0.0-20201208030716-90b105fa0258
+	github.com/nevata/txtcolor v0.0.0-20180402125809-7bc901f6614f
+	gorm.io/driver/sqlite v1.1.6
+	gorm.io/gorm v1.21.16
+)
--- model.go
+++ model.go
@@ -4,9 +4,12 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
+	"log"
 	"strconv"
 	"strings"
 	"time"
+
+	"gorm.io/gorm"
 )
 
 //Model 基本模型的定义
@@ -25,11 +28,11 @@ type DevOrder struct {
 }
 
 //OrderByBuild 根据ID返回排序字段
-func OrderByBuild(db *sql.DB, id uint) string {
-	row := db.QueryRow("SELECT columns FROM dev_orders WHERE id = ?", id)
+func OrderByBuild(db *gorm.DB, id uint) string {
+	row := db.Raw("SELECT columns FROM dev_orders WHERE id = ?", id)
 	var orderby string
-	err := row.Scan(&orderby)
-	if err == sql.ErrNoRows {
+	err := row.Scan(&orderby).Error
+	if err == gorm.ErrRecordNotFound {
 		return ""
 	}
 	if err != nil {
@@ -46,91 +49,144 @@ type DevWhere struct {
 	Desc       string //备注
 }
 
+type ValueType uint8
+
+const (
+	VTInt      ValueType = iota + 1 //1 int
+	VTFloat                         //2 float
+	VTString                        //3 string
+	VTDate                          //4 date
+	VTTime                          //5 time
+	VTDateTime                      //6 datetime
+	VTStringEx                      //7 %string%
+	VTSubQuery                      //8
+)
+
+type Condition struct {
+	Key         string       `json:"key"`
+	Val         string       `json:"val"`
+	VT          ValueType    `json:"vt"`
+	KeyRequired bool         `json:"key_required"` //必传参数
+	ValRequired bool         `json:"val_required"` //必加条件
+	Conditions  []*Condition `json:"conditions"`
+}
+
+func encodeJson(v interface{}) string {
+	buf, err := json.Marshal(v)
+	if err != nil {
+		panic(err)
+	}
+	return string(buf)
+}
+
+func parseCondition(
+	condition *Condition,
+	keys map[string]string,
+	val string,
+	builder *strings.Builder,
+	vals *[]interface{},
+) error {
+	switch condition.VT {
+	case VTInt:
+		v, e := strconv.ParseInt(val, 10, 64)
+		if e != nil {
+			return fmt.Errorf("参数[%s]类型不正确", condition.Key)
+		}
+		*vals = append(*vals, sql.Named(condition.Key, v))
+	case VTFloat:
+		v, e := strconv.ParseFloat(val, 64)
+		if e != nil {
+			return fmt.Errorf("参数[%s]类型不正确", condition.Key)
+		}
+		*vals = append(*vals, sql.Named(condition.Key, v))
+	case VTDate:
+		v, e := time.Parse(dateLayout, val)
+		if e != nil {
+			return fmt.Errorf("参数[%s]类型不正确", condition.Key)
+		}
+		*vals = append(*vals, sql.Named(condition.Key, v))
+	case VTTime:
+		v, e := time.Parse("15:04:05", val)
+		if e != nil {
+			return fmt.Errorf("参数[%s]类型不正确", condition.Key)
+		}
+		*vals = append(*vals, sql.Named(condition.Key, v))
+	case VTDateTime:
+		v, e := time.Parse(timeLayout, val)
+		if e != nil {
+			return fmt.Errorf("参数[%s]类型不正确", condition.Key)
+		}
+		*vals = append(*vals, sql.Named(condition.Key, v))
+	case VTStringEx:
+		*vals = append(*vals, sql.Named(condition.Key, "%"+val+"%"))
+	case VTSubQuery:
+		return parseConditions(condition.Conditions, keys, builder, vals)
+	default:
+		*vals = append(*vals, sql.Named(condition.Key, val))
+	}
+
+	return nil
+}
+
+func parseConditions(
+	conditions []*Condition,
+	keys map[string]string,
+	builder *strings.Builder,
+	vals *[]interface{},
+) error {
+	for _, condition := range conditions {
+		val, ok := keys[condition.Key]
+		if condition.KeyRequired && !ok {
+			return fmt.Errorf("[%s]必传参数", condition.Key)
+		}
+		if condition.ValRequired || ok {
+			if builder.Len() > 0 {
+				builder.WriteString(" AND ")
+			}
+			builder.WriteString(condition.Val)
+			if ok {
+				if err := parseCondition(
+					condition, keys, val, builder, vals); err != nil {
+					return err
+				}
+				log.Println("parseConditions:", vals)
+			}
+		}
+	}
+	return nil
+}
+
 //WhereBuild 根据ID返回过滤条件
-func WhereBuild(db *sql.DB, id uint, keys map[string]string) (string, []interface{}, error) {
-	row := db.QueryRow("SELECT conditions FROM dev_wheres WHERE id = ?", id)
+func WhereBuild(
+	db *gorm.DB,
+	id uint,
+	keys map[string]string,
+) (
+	string,
+	[]interface{},
+	error,
+) {
+	row := db.Raw("SELECT conditions FROM dev_wheres WHERE id = ?", id)
 	var s string
-	err := row.Scan(&s)
-	if err == sql.ErrNoRows {
+	err := row.Scan(&s).Error
+	if err == gorm.ErrRecordNotFound {
 		return "", nil, nil
 	}
 	if err != nil {
 		return "", nil, err
 	}
 
-	type ValueType uint8
-
-	const (
-		VTInt      ValueType = iota + 1 //1 int
-		VTFloat                         //2 float
-		VTString                        //3 string
-		VTDate                          //4 date
-		VTTime                          //5 time
-		VTDateTime                      //6 datetime
-		VTStringEx                      //7 %string%
-	)
-
-	var conditions []struct {
-		Key      string    `json:"key"`
-		Val      string    `json:"val"`
-		VT       ValueType `json:"vt"`
-		Required bool      `json:"required"`
-	}
-
+	var conditions []*Condition
 	err = json.NewDecoder(strings.NewReader(s)).Decode(&conditions)
 	if err != nil {
 		return "", nil, err
 	}
 
-	where := ""
+	builder := strings.Builder{}
 	vals := []interface{}{}
-	for _, condition := range conditions {
-		val, ok := keys[condition.Key]
-		if condition.Required || ok {
-			if where != "" {
-				where += " AND "
-			}
-			where += condition.Val
-			if ok {
-				switch condition.VT {
-				case VTInt:
-					v, e := strconv.ParseInt(val, 10, 64)
-					if e != nil {
-						return "", nil, fmt.Errorf("参数[%s]类型不正确", condition.Key)
-					}
-					vals = append(vals, sql.Named(condition.Key, v))
-				case VTFloat:
-					v, e := strconv.ParseFloat(val, 64)
-					if e != nil {
-						return "", nil, fmt.Errorf("参数[%s]类型不正确", condition.Key)
-					}
-					vals = append(vals, sql.Named(condition.Key, v))
-				case VTDate:
-					v, e := time.Parse(dateLayout, val)
-					if e != nil {
-						return "", nil, fmt.Errorf("参数[%s]类型不正确", condition.Key)
-					}
-					vals = append(vals, sql.Named(condition.Key, v))
-				case VTTime:
-					v, e := time.Parse("15:04:05", val)
-					if e != nil {
-						return "", nil, fmt.Errorf("参数[%s]类型不正确", condition.Key)
-					}
-					vals = append(vals, sql.Named(condition.Key, v))
-				case VTDateTime:
-					v, e := time.Parse(timeLayout, val)
-					if e != nil {
-						return "", nil, fmt.Errorf("参数[%s]类型不正确", condition.Key)
-					}
-					vals = append(vals, sql.Named(condition.Key, v))
-				case VTStringEx:
-					vals = append(vals, sql.Named(condition.Key, "%"+val+"%"))
-				default:
-					vals = append(vals, sql.Named(condition.Key, val))
-				}
-			}
-		}
+	if err := parseConditions(conditions, keys, &builder, &vals); err != nil {
+		return "", nil, err
 	}
 
-	return where, vals, nil
+	return builder.String(), vals, nil
 }
